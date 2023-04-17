1054e63162c24473771d7678c216e4f44403f713
Brooks <brooks@pdrutah.com>
2023-04-17 15:59:04 -0600

    init

---
+++ notes.md
@@ -0,0 +1,11 @@
+# Notes
+
+## Data Types
+
+Every value has a data type. Each column has a designated type.
+
+- Numbers: serial, integer, numeric, double precision `sql SELECT (2.0::INTEGER)`
+- Characters: char, varchar, varchar(40), text
+- Boolean: true, false
+- Date/Time: date, time, time with time zone, timestamp
+- Interval: dates that can be operated on
---
+++ products.sql
@@ -0,0 +1,31 @@
+-- CREATE TABLE products (
+-- 	id SERIAL PRIMARY KEY,
+-- 	name VARCHAR(40),
+-- 	department VARCHAR(40),
+-- 	price INTEGER,
+-- 	weight INTEGER
+-- );
+
+-- UPDATE products
+-- SET price = 999
+-- WHERE price = 0;
+
+-- ALTER TABLE products
+-- ALTER COLUMN price
+-- SET NOT NULL;
+
+-- ALTER TABLE products
+-- ALTER COLUMN price
+-- SET DEFAULT 999;
+
+-- ALTER TABLE products
+-- ADD UNIQUE (name);
+
+-- INSERT INTO products (name, department, price, weight)
+-- VALUES ('Shirt', 'Clothes', 100, 50), ('Pants', 'Clothes', 150, 100), ('Shoes', 'Clothes', 75, 25);
+
+SELECT * FROM products;
+
+
+
+
